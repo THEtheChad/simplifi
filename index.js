5b021c14fe6d05826532d8acd81be3446457f8a1
THEtheChad <chad.d.elliott@gmail.com>
2016-11-10 09:34:29 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var request = require('request');
+
+function API(config){
+	if(!(this instanceof API)) return new API(config);
+
+	this.app_key = config.app_key;
+	this.user_key = config.user_key;
+}
+var api = API.prototype = {};
+
+api._http = function(endpoint, callback){
+	request({
+	  url: 'https://app.simpli.fi/api/' + endpoint,
+	  headers: {
+	  	'X-App-Key': this.app_key,
+	  	'X-User-Key': this.user_key,
+	  	'Content-Type': 'application/json'
+	  }
+	}, function(err, res, body){
+		if(res.statusCode != 200) return console.err(res.statusCode, res.statusMessage);
+
+		callback(body);
+	});
+};
+
+api.adFileTypes = function(callback){
+	this._http('ad_file_types', callback);
+};
+
+api.me = function(callback){
+	this._http('', callback);
+};
+
+module.exports = API;
