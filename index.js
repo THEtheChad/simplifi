b5d88138f4f95b925431f30de6da2f385de1fd5f
THEtheChad <chad.d.elliott@gmail.com>
2016-11-15 14:11:29 -0600

    updated api to emitter subclass and added streaming capabilities

--- index.js
+++ index.js
@@ -1,66 +1,189 @@
-var https = require('https');
-var makeSource = require('stream-json');
-var Assembler  = require('stream-json/utils/Assembler');
+const https = require('https');
+const stream = require('stream');
+const Combo = require('stream-json/Combo');
+const StreamArray = require('stream-json/utils/StreamArray');
+const EventEmitter = require('events');
 
-function API(config){
-	if(!(this instanceof API)) return new API(config);
+class FilterRecords extends stream.Transform {
+  constructor(config) {
+  	config || (config = {});
+  	config.objectMode = true;
+    super(config);
 
-	this.app_key = config.app_key;
-	this.user_key = config.user_key;
+    this.id = config.id;
+    this.type = config.type;
+    this.match = false;
+    this.array_stack = [];
+  }
+
+  _transform(chunk, enc, next) {
+  	if(this.match){
+  		this.push(chunk);
+
+  		if(chunk.name == 'startArray'){
+  			this.array_stack.push(true);
+  		}
+  		if(chunk.name == 'endArray'){
+  			this.array_stack.pop();
+  		}
+  		if(!this.array_stack.length){
+  			this.match = false;
+  		}
+  	}
+
+  	if(!this.array_stack.length && chunk.name == 'keyValue'){
+  		if(chunk.value == this.type) this.match = true;
+  	}
+  	next();
+  }
 }
-var api = API.prototype = {};
 
-api.get = function(endpoint, callback){
-	return https.get({
-		host: 'app.simpli.fi',
-		path: '/api/' + endpoint,
-		headers: {
-			'X-App-Key': this.app_key,
-			'X-User-Key': this.user_key,
-			'Content-Type': 'application/json'
-		}
-	}, callback);
-};
+class SimpleRecords extends stream.Transform {
+  constructor(config) {
+  	config || (config = {});
+  	config.objectMode = true;
+    super(config);
+  }
 
-api.companies = function(id, callback){
-	if(arguments.length < 2) callback = id;
-	var recordParser = this._recordParser(callback):
-	this.get('companies', (response)=>response.pipe(recordParser))
-};
+  _transform(chunk, enc, next) {
+  	this.push(chunk.value);
+  	next();
+  }
+}
 
-api._recordParser = function(record){
-	var source = makeSource();
-	var assembler;
-	var array_stack = [];
-	var object_stack = [];
+function findRecords(type, response){
+	let combo = new Combo({packKeys:true,packStrings:true,packNumbers:true});
+	let filter = new FilterRecords({type:type});
+	let buildRecords = new StreamArray();
+	let simpleRecords = new SimpleRecords();
 
-	source.output.on('data', (chunk)=>{
-		if(chunk.name == 'endArray'){
-			array_stack.pop();
-		}
+	return response.pipe(combo).pipe(filter).pipe(buildRecords).pipe(simpleRecords);
+}
 
-		if(array_stack.length){
-			if(chunk.name == 'startObject'){
-				object_stack.push(1);
-				if(object_stack.length == 1) assembler = new Assembler();
+function extend(trgt, src){
+	for(var k in src){
+		trgt[k] = src[k]
+	}
+	return trgt;
+}
+
+
+class API extends EventEmitter {
+	constructor(config) {
+		super();
+
+		this.app_key = config.app_key;
+		this.user_key = config.user_key;
+
+		this.type = config.type || 'root';
+		this.ids = config.ids || [];
+		this.parent = config.parent || this;
+	}
+
+	get(endpoint, parser) {
+		console.log(endpoint);
+		return https.get({
+			host: 'app.simpli.fi',
+			path: '/api/' + endpoint,
+			headers: {
+				'X-App-Key': this.app_key,
+				'X-User-Key': this.user_key,
+				'Content-Type': 'application/json'
 			}
+		}, (response)=>{
+	    if (response.statusCode < 200 || response.statusCode > 299) {
+	    	console.error(new Error(response.statusCode + ': ' + response.statusMessage));
+	    }
+	    else{
+	    	// response.on('end', ()=>{console.log('closed:' + endpoint)});
+	    	parser(response);
+	    }
+		});
+	}
 
-			assembler[chunk.name] && assembler[chunk.name](chunk.value);
+	_new(config) {
+		if(config.ids){
+			if(!Array.isArray(config.ids)) config.ids = [config.ids];
 
-			if(chunk.name == 'endObject'){
-				object_stack.pop();
-				if(!object_stack.length){
-					record(assembler.current);
+			if(config.ids.length > 0){
+				if(this.ids.length > 1){
+					return console.error(new Error('You can only specify 1 ' + this.type + ' when specifying 1 or more ' + config.type + '.'));
 				}
 			}
 		}
 
-		if(chunk.name == 'startArray'){
-			array_stack.push(1);
-		}
-	});
+		let obj = new API({
+			app_key: this.app_key,
+			user_key: this.user_key,
+			ids: config.ids,
+			type: config.type,
+			parent: this
+		});
 
-	return source.input;
-};
+		return obj;
+	}
+
+	companies(ids){
+		return this._new({
+			ids: ids,
+			type: 'companies'
+		});
+	}
+
+	clients(ids){
+		return this._new({
+			ids: ids,
+			type: 'clients'
+		});
+	}
+
+	campaigns(ids){
+		return this._new({
+			ids: ids,
+			type: 'campaigns'
+		});
+	}
+
+	endpoint(parent_record, record_id){
+		let endpoint = [];
+
+		if(parent_record.resource){
+			let prefix = /api\/(.+)$/.exec(parent_record.resource)[1];
+			endpoint.push(prefix);
+		}
+
+		endpoint.push(this.type);
+
+		(record_id != null) && endpoint.push(record_id);
+
+		return endpoint.join('/');
+	}
+
+	_eachId(action){
+		(this.ids.length == 0) ? action('') : this.ids.forEach(action);
+	}
+
+	each(callback){
+		return this.on('record', callback);
+	}
+
+	fetch(){
+		if(this.type == 'root') return this.emit('record', {});
+
+		this.parent.each((record)=>{
+			this._eachId((id)=>this.get(this.endpoint(record, id), (response)=>findRecords(this.type, response).on('data', (data)=>this.emit('record', data))))
+		}).fetch();
+	}
+}
+// function API(config){
+// 	if(!(this instanceof API)) return new API(config);
+
+// 	this.app_key = config.app_key;
+// 	this.user_key = config.user_key;
+
+// 	this.type = config.type || 'root';
+// 	this.ids = config.ids || [];
+// 	this.parent = config.parent || this;
+// }
 
 module.exports = API;
