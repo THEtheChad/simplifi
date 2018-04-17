91325cada8c1d0b3e9b5ca772e94604e9de4b7fe
THEtheChad <chad.d.elliott@gmail.com>
2018-04-17 11:31:51 -0500

    pulled toJSON out into its own file

---
+++ src/toJSON.js
@@ -0,0 +1,18 @@
+export default class toJSON extends stream.Transform {
+  constructor(opts = {}) {
+    super(Object.assign(opts, { objectMode: true }))
+    this.body = []
+  }
+
+  _flush(done) {
+    const body = this.body.join('')
+    const json = JSON.parse(body)
+    this.push(json)
+    done()
+  }
+
+  _transform(chunk, enc, next) {
+    this.body.push(chunk.toString())
+    next()
+  }
+}
